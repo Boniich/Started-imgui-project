ef8a2c56e1054a6f558b6f2173d0914e0eae21ad
boniich <ezequieldbo25@gmail.com>
2023-07-01 14:01:47 -0300

    feat: set all necessary to fonts and icons works

--- StartedIMGUIBaseCode/main.cpp
+++ StartedIMGUIBaseCode/main.cpp
@@ -1,12 +1,13 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui-SFML.h"
-
+#include "Icons/IconsFontAwesome5.h"
 #include <SFML/Graphics.hpp>
 
 bool my_tool_active = true;
 
 int main()
 {
+    ImGui::CreateContext();
 
     bool circleExists = true;
     float circleRadius = 200.f;
@@ -26,6 +27,31 @@ int main()
    // shape.setFillColor(sf::Color::Green);
 
     sf::Clock deltaClock;
+
+    //fonts and icons
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    float baseFontSize = 32.0f; // 13.0f is the size of the default font. Change to the font size you use.
+    float iconFontSize = baseFontSize * 2.0f / 3.0f; // FontAwesome fonts need to have their sizes reduced by 2.0f/3.0f in order to align correctly
+
+    static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_16_FA, 0 };
+    ImFontConfig icons_config;
+    icons_config.MergeMode = true;
+    icons_config.PixelSnapH = true;
+    icons_config.GlyphMinAdvanceX = iconFontSize;
+
+    io.Fonts->Clear(); // clear fonts if you loaded some before (even if only default one was loaded)
+    io.Fonts->AddFontFromFileTTF("Fonts\\Roboto-Regular.ttf", 15.f);
+    io.Fonts->AddFontFromFileTTF(FONT_ICON_FILE_NAME_FAS, iconFontSize, &icons_config, icons_ranges);
+    ImFont* robotoFont12px = io.Fonts->AddFontFromFileTTF("Fonts\\Roboto-Regular.ttf", 12.f);
+    ImFont* robotoFont18px = io.Fonts->AddFontFromFileTTF("Fonts\\Roboto-Regular.ttf", 18.f);
+
+    ImGui::SFML::UpdateFontTexture(); // important call: updates font texture
+
+
+    ImGui::NewFrame();
+    ImGui::Render();
     while (window.isOpen())
     {
         sf::Event event;
@@ -56,8 +82,8 @@ int main()
 
         shape.setRadius(circleRadius);
         shape.setPointCount(circleSegments);
-
         window.clear();
+
         if (circleExists)
         {
             window.draw(shape);
@@ -67,7 +93,9 @@ int main()
         window.display();
     }
 
+    ImGui::EndFrame();
     ImGui::SFML::Shutdown();
+    ImGui::DestroyContext();
 
     return 0;
 }
